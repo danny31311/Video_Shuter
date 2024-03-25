38aa997cf34f3bf2187495b1c14f942a05f9b7ba
danny31311 <102857937+danny31311@users.noreply.github.com>
2024-03-25 07:58:40 +0300

    Create pers.py

---
+++ program/pers.py
@@ -0,0 +1,23 @@
+class Personaj:
+    def __init__(self):
+      #Инициализация модуля
+
+    def UpdatePers(self):
+      #Модуль обновления персонажа данных о нём
+
+class Moving:
+    def __init__(self):
+      #Инициализация модуля движения
+
+    def UpdateMoving(self):
+      #Модуль обновления нахождения персонажа
+
+    def MovingWithUnstoping(self):
+      #Модуль движения без возможности остановиться
+
+class Game_camera:
+    def __init__(self):
+      #Инициализация модуля игровой камеры
+
+    def UpdateCamera(self):
+      #Модуль обновления положения игровой камеры
