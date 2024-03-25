6142772f6b7967996f7b2d3ad494d4358999d47d
danny31311 <102857937+danny31311@users.noreply.github.com>
2024-03-25 10:00:06 +0300

    Update save_point.py

--- program/save_point.py
+++ program/save_point.py
@@ -1,1 +1,9 @@
+class Save_Points:
+  def __init__(self):
+    #Инициализация класса точек сохранения
 
+  def Do_Save_Point(self):
+    #Модуль сохранения точки сохранения
+
+  def Change_Save_Point(self):
+    #Изменение точки сохранения
