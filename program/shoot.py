9216515b4a7be15fba7bf81b5ecfdc6bd3ec55b3
danny31311 <102857937+danny31311@users.noreply.github.com>
2024-03-25 08:00:52 +0300

    Create shoot.py

---
+++ program/shoot.py
@@ -0,0 +1,16 @@
+class Cursor:
+  def __init__(self):
+    #Инициализция класса
+
+  def UpdateCursor(self):
+    #Модуль обновления положения курсора
+
+class Shooting:
+  def __init__(self):
+    #Иницилазация класса
+
+  def ChangeShooting(self):
+    #Модуль изменения стрельбы
+
+  def ShowVectorShoot(self):
+    #Модуль показа вектора стрельбы
